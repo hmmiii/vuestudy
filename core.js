163496d252ffda37158502e8bb6cbc1edf679d4e
hmmi <hmmi@kakao.com>
2023-06-04 13:55:39 +0900

    테일윈드

---
+++ core.js
@@ -0,0 +1,256 @@
+var MyBoardList = {
+    props:['db'],
+    template:`
+    <div>
+            <table class="table border-collapse border-solid border border-indigo-800">
+                <colgroup>
+                    <col style="width: 10%;">
+                    <col>
+                    <col style="width: 10%">
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th>글번호</th>
+                        <th>글제목</th>
+                        <th>작성자</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in db" class="border border-solid border-gray-300">
+                        <td>{{item.no}}</td>
+                        <td v-on:click="boardRead(item)" style="cursor:pointer">{{item.title}}</td>
+                        <td>{{item.writer}}</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="3"><button @click="boardWrite" type="button" class="btn btn-primary">글쓰기</button></td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>            
+    `,
+    methods:{
+        boardRead:function(info){
+            this.$emit('board-read',info);
+        },
+        boardWrite:function(){
+            this.$emit('board-write');
+        }
+    }
+};
+
+var MyBoardRead = {
+    props:["db"],
+    template:`
+    <div>
+        <table class="table">
+            <colgroup>
+                <col style="width: 10%;">
+                <col>
+                <col style="width: 10%">
+            </colgroup>
+            <thead>
+                <tr>
+                    <th>글번호 : {{db.no}}</th>
+                    <th>글제목 : {{db.title}}</th>
+                    <th>작성자 : {{db.writer}}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td colspan="3">
+                        <p>{{db.content}}</p>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td><button type="button" class="btn btn-secondary" @click="boardModify(db.no)">수정</button></td>
+                    <td><button type="button" class="btn btn-danger" @click="boardDelete(db.no)">삭제</button></td>
+                    <td style="text-align: right;"><button type="button" @click="boardList" class="btn btn-success">목록</button></td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>            
+    `,
+    methods:{
+        boardList:function(){
+            this.$emit('board-list');
+        },
+        boardDelete:function(no){
+            this.$emit('board-delete',no);
+        },
+        boardModify:function(no){
+            this.$emit('board-modify',no);
+        }
+    }
+};
+
+var MyBoardWrite = {
+    data:function(){
+       return {
+        no:0,
+        title:'',
+        writer:'',
+        content:''
+       } 
+    },
+    template: `
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th><input type="text" placeholder="글제목" style="width: 100%;" v-model="title"></th>
+                    <th><input type="text" placeholder="글쓴이" style="width: 100%;" v-model="writer"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td colspan="2">
+                        <textarea style="width:100%" placeholder="글내용" v-model="content"></textarea>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td><button @click="boardSave" type="button" class="btn btn-primary">등록</button></td>
+                    <td style="text-align: right;"><button type="button" @click="boardList" class="btn btn-success" >목록</button></td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>            
+    `,
+    methods:{
+        boardList : function(){
+            this.$emit('board-list');
+        },
+        boardSave : function(){
+            this.$emit('board-save',{no:0,title:this.title,writer:this.writer,content:this.content})
+        }
+    }
+};
+
+var MyBoardModify = {
+    props:['info'],
+    template: `
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th><input type="text" placeholder="글제목" style="width: 100%;" v-model="info.title"></th>
+                    <th><input type="text" placeholder="글쓴이" style="width: 100%;" v-model="info.writer" disabled="true"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td colspan="2">
+                        <textarea style="width:100%" placeholder="글내용" v-model="info.content"></textarea>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td><button @click="boardModifyDone(info.no, info);" type="button" class="btn btn-primary">수정완료</button></td>
+                    <td style="text-align: right;"><button type="button" @click="boardList" class="btn btn-success" >목록</button></td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>            
+    `,
+    methods:{
+        boardModifyDone : function(no,info){
+            this.$emit('board-modify-done',no,info);
+        },
+        boardList:function(){
+            this.$emit('board-list');
+        },
+    }
+};
+
+
+
+new Vue({
+    el: '#app',
+    data:{
+        boardData : [
+            {
+                no:0,
+                title:'글1',
+                content:'내용1',
+                writer:'작성자1'
+            },
+            {
+                no:1,
+                title:'글2',
+                content:'내용2',
+                writer:'작성자2'
+            },
+            {
+                no:2,
+                title:'글3',
+                content:'내용3',
+                writer:'작성자3'
+            },
+        ],
+        listOK:true,
+        readOK:false,
+        writeOK:false,
+        modifyOK:false,
+        info:{}
+    },
+    methods:{
+        boardRead:function(info){
+            this.info = info;
+            this.readOK = true;
+            this.listOK = false;
+        },
+        boardList:function(){
+            this.listOK = true;
+            this.readOK = false;
+            this.writeOK = false;
+            this.modifyOK = false;
+        },
+        boardWrite:function(){
+            this.listOK = false;
+            this.writeOK = true;
+        },
+        boardSave:function(info){
+            var copy = {...info};
+            copy.no = this.boardData.length != 0 ? this.boardData[this.boardData.length-1].no + 1 : 0;
+            this.boardData.push(copy);
+            this.writeOK = false;
+            this.listOK = true;
+        },
+        boardDelete:function(no){
+            var target = this.boardData.filter(function(a){
+                return a.no == no;
+            });
+            var targetIndex = this.boardData.findIndex(function(a){
+                return a.no == target.no;
+            });
+            this.boardData.splice(targetIndex,1);
+            this.readOK = false;
+            this.listOK = true;
+            console.log(this.boardData)
+        },
+        boardModify:function(no){
+            this.readOK = false;
+            this.modifyOK = true;
+        },
+        boardModifyDone:function(no,info){
+            console.log(no,info);
+            var copy = {...info};
+            this.boardData[no] = copy;
+            this.modifyOK = false;
+            this.listOK = true;
+        }
+    },
+    components:{
+        'my-board-list':MyBoardList,
+        'my-board-read':MyBoardRead,
+        'my-board-write':MyBoardWrite,
+        'my-board-modify':MyBoardModify
+    }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // 기본적인 Tailwind CSS 설정...
+  
+    // CDN으로 가져온 Tailwind CSS URL 설정
+    // 여기에 사용하는 CDN URL을 입력하세요
+    purge: [],
+    theme: {},
+    variants: {},
+    plugins: [],
+  };
+  
